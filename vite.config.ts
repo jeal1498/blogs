dc6b509c303950eebf882dd1e7d7d79f0cba8daa
Eduardo.Alv <jeal1498@gmail.com>
2026-02-21 11:38:52 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+// @ts-ignore
+import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      staticDir: path.join(__dirname, "dist"),
+      routes: [
+        "/",
+        "/evaluacion-tdah-ninos",
+        "/evaluacion-tdah-adultos",
+        "/evaluacion-autismo-cancun",
+        "/blog/cuanto-cuesta-valoracion-tdah-cancun",
+        "/blog/tdah-en-ninas-sintomas",
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
